adbf63248f7d9d2dace8b634c13ee9e4c3a19223
RATIONAL331 <rational331@gmail.com>
2019-02-18 22:46:08 +0900

    Update 1912.cpp

--- 1912/1912/1912.cpp
+++ 1912/1912/1912.cpp
@@ -1,28 +1,39 @@
 #include<iostream>
 #include<algorithm>
-int calculate(int * numArr, int* dp, int A, int B) {
-	for (int i = A; i <= B; i++) {
-		dp[i] = std::max(std::max(dp[i - 1] + numArr[i], dp[i - 1]), numArr[i]);
+int getMax(int* arr, int n) {
+	int max = arr[1];
+	for (int i = 2; i <= n; i++) {
+		max = std::max(max, arr[i]);
 	}
-	return dp[B];
+	return max;
+}
+void calculate(int * numArr, int * dp, int A, int B) {
+	for (int i = A; i <= B; i++) {
+		if (dp[i - 1] < 0) dp[i] = numArr[i];
+		else dp[i] = dp[i - 1] + numArr[i];
+	}
 }
  // 메모리 제한 해결하기
 int main() {
 	int n;
 	std::cin >> n;
-
+	// Input Phase;
 	int num;
 	int * numArr = new int[n + 1];
-	int * dp = new int[n + 1];
 	for (int i = 1; i <= n; i++){
 		std::cin >> num;
 		numArr[i] = num;
 	}
+	// DP init
+	int * dp = new int[n + 1];
 	for (int col = 0; col < n + 1; col++) {
 		dp[col] = INT32_MIN;
 	}
-	dp[1] = numArr[1];
-	std::cout<<calculate(numArr, dp, 2, n);
+	dp[0] = 0;
+	// Calcualte
+	calculate(numArr, dp, 1, n);
+	std::cout << getMax(dp, n);
+	// Delete Phase;
 	delete[] dp;
 	delete[] numArr;
 	return 0;
