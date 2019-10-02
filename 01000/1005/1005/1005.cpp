b14d32dbedddbcfb5944cb69142d2f28df4ea8df
RATIONAL331 <rational331@gmail.com>
2019-10-02 20:10:08 +0900

    1005

---
+++ 01000/1005/1005/1005.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	int N, K, W;
+	while (T--) {
+		cin >> N >> K;
+		
+		vector<int> cost(K);
+		for (int i = 0; i < K; i++)
+			cin >> cost[i];
+
+
+
+		cin >> W;
+	}
+	return 0;
+}
