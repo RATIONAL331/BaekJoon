eb883db4b74e3299bbd918e772311f37c8870475
RATIONAL331 <rational331@gmail.com>
2019-10-11 15:10:55 +0900

    Update 15681.cpp

--- 15000/15681/15681/15681.cpp
+++ 15000/15681/15681/15681.cpp
@@ -1,14 +1,30 @@
 #include<iostream>
 #include<vector>
-#include<algorithm>
 using namespace std;
 
 int N, R, Q;
-int solve(vector<vector<int>>& tree, int node) {
-	const vector<int>& treeRoot = tree[R - 1];
+vector<int> dp;
+vector<bool> visited;
 
+int solve(vector<vector<int>>& tree, int search) {
+	int& result = dp[search];
+	if (result != 0) return result;
+
+	result = 1;
+	for (const int& child : tree[search]) {
+		if (visited[child] == false) {
+			visited[child] = true;
+			result += solve(tree, child);
+		}
+	}
+	return result;
 }
+
 int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
 	cin >> N >> R >> Q;
 	vector<vector<int>> tree(N);
 
@@ -19,10 +35,16 @@ int main() {
 		tree[V - 1].push_back(U - 1);
 	}
 
+	dp.resize(N);
+	visited.resize(N);
+
+	visited[R - 1] = true;
+	solve(tree, R - 1);
+
 	int solveNode;
 	for (int i = 0; i < Q; i++) {
 		cin >> solveNode;
-		cout << solve(tree, solveNode - 1);
+		cout << solve(tree, solveNode - 1) << endl;
 	}
 
 	return 0;
