85e183c522a463b1797dbdef57b95c713d6944af
RATIONAL331 <rational331@gmail.com>
2019-10-11 17:50:15 +0900

    16637

---
+++ 16000/16637/16637/16637.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<vector>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int maxValue = INT32_MIN;
+
+int solve(int idx) {
+
+	
+}
+
+int main(){
+	int N;
+	string str;
+	cin >> N >> str;
+	cout << solve(0);
+
+	return 0;
+}
