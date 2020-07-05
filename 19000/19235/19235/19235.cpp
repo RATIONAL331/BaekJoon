1ccafb0c0921031d1f28f00d5ef3177b71d53074
RATIONAL331 <rational331@gmail.com>
2020-07-05 21:12:19 +0900

    19235

---
+++ 19000/19235/19235/19235.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<deque>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	
+	int t, x, y;
+	while (N--) {
+		cin >> t >> x >> y;		
+		// 블록을 놓는다.	
+		switch (t) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		}
+		
+		// 점수를 얻을 수 있는 지 확인한다.
+		
+		
+
+		
+		// 보드 바깥으로 나와있는지 확인한다.
+		
+
+	}
+	return 0;
+}
